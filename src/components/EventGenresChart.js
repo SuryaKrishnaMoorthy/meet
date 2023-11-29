e94cd41774481abde0ab74dbda13e2db956a1d3a
Surya Krishna Moorthy <37917817+SuryaKrishnaMoorthy@users.noreply.github.com>
2023-11-29 12:03:30 -0500

    add css for charts and percentage values for pie chart

---
+++ src/components/EventGenresChart.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import { ResponsiveContainer, PieChart, Pie, Legend } from "recharts";
+
+const data = [
+  { name: "Group A", value: 400 },
+  { name: "Group B", value: 300 },
+  { name: "Group C", value: 300 },
+  { name: "Group D", value: 200 },
+];
+const EventGenresChart = ({ events }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(getData());
+  }, [`${events}`]);
+
+  const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
+
+  const getData = () => {
+    const data = genres.map((genre) => {
+      const filteredEvents = events.filter((event) =>
+        event.summary.includes(genre)
+      );
+      return { name: genre, value: filteredEvents.length };
+    });
+    return data;
+  };
+
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const RADIAN = Math.PI / 180;
+    const radius = outerRadius;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
+    const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
+    return percent ? (
+      <text
+        x={x}
+        y={y}
+        fill="#8884d8"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+      </text>
+    ) : null;
+  };
+
+  return (
+    <div style={{ width: "100%", height: 400 }}>
+      <ResponsiveContainer>
+        <PieChart>
+          <Pie
+            dataKey="value"
+            nameKey="name"
+            data={data}
+            fill="#8884d8"
+            label={renderCustomizedLabel}
+            labelLine={false}
+            outerRadius={130}
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default EventGenresChart;
